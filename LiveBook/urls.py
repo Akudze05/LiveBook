b258dda212da45602fa5f7a9411160acaabc799a
Akudze05 <akudze05@gmail.com>
2026-07-07 10:13:59 +0300

    Sparkle effects and datetime

--- LiveBook/urls.py
+++ LiveBook/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+"""
+СПбГУ
+ИТМО
+"""
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
--- main/views.py
+++ main/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
 
 def index(request):
-    return render(request, 'main/main.html')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'main/main.html', context)
 
